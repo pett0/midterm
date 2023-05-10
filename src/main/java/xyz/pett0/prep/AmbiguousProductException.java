3c398bd2cb67fa4904fad374088571e6cc0f03f0
hatevery1 <llol4990@gmail.com>
2023-05-10 19:27:21 +0200

    initial commit

--- src/main/java/xyz/pett0/prep/AmbiguousProductException.java
+++ src/main/java/xyz/pett0/prep/AmbiguousProductException.java
@@ -2,19 +2,19 @@ package xyz.pett0.prep;
 
 import java.util.List;
 
-public class AmbigiousProductException extends Exception {
+public class AmbiguousProductException extends Exception {
     private final List<String> productList;
-    public AmbigiousProductException(List<String> productList) {
+    public AmbiguousProductException(List<String> productList) {
         this.productList = productList;
     }
     @Override
     public String getMessage() {
-        return "Ambigious product found: " + productList;
+        return "Ambiguous product found: " + productList;
     }
 
     @Override
     public void printStackTrace() {
-        System.err.println("AmbigiousProductException: " + getMessage());
+        System.err.println("AmbiguousProductException: " + getMessage());
         for (StackTraceElement element : getStackTrace()) {
             System.err.println("\tat " + element);
         }
